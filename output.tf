035c21ba434bf73d98aa4da21fe278607a37de53
Jarret Nachtigal <jarretn1224@gmail.com>
2023-03-03 12:47:30 -0500

    output added

---
+++ output.tf
@@ -0,0 +1,3 @@
+output "azuread_my_user_prinicpal_name" {
+  value = azuread_user.my_user.user_principal_name
+}
--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region = "us-east-1"
-  # access_key = ""
-  # secret_key = ""
+  region     = "us-east-1"
+  access_key = "AKIAZOX2VG2VPBRTMLGQ"
+  secret_key = "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"
 }
 
 provider "azurerm" {
