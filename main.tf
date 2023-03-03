724564d5a3ca01967bf70db656336c4e9bc1dbe1
Jarret Nachtigal <jarretn1224@gmail.com>
2023-03-03 11:54:38 -0500

    added a virtual machine along with its dependencies

--- main.tf
+++ main.tf
@@ -48,6 +48,7 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
+# resource group
 resource "azurerm_resource_group" "resource_group_1" {
   name     = var.resource_group_name
   location = "eastus"
@@ -57,14 +58,76 @@ resource "azurerm_resource_group" "resource_group_1" {
   }
 }
 
+# virtual machine dependency
+resource "azurerm_virtual_network" "main" {
+  name                = "my-vet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.resource_group_1.location
+  resource_group_name = azurerm_resource_group.resource_group_1.name
+}
+
+# virtual machine dependency
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.resource_group_1.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# virtual machine dependency
+resource "azurerm_network_interface" "main" {
+  name                = "my-nic"
+  location            = azurerm_resource_group.resource_group_1.location
+  resource_group_name = azurerm_resource_group.resource_group_1.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# virtual machine
+resource "azurerm_virtual_machine" "main" {
+  name                  = "my-vm"
+  location              = azurerm_resource_group.resource_group_1.location
+  resource_group_name   = azurerm_resource_group.resource_group_1.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
 # done - my user
 # done - trainer user 
 # done - 4 for_each users
 # done - 2 s3 buckets
 # resource group on azure with a 
-# - vm
+# done - vm
 # - storage account
-# 2 tags
+# done - 2 tags
 # 1 output variable
 # 5 variables 
 
