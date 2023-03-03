5fb2233b8d9ce2954093ce27abbd34e64930511d
Jarret Nachtigal <jarretn1224@gmail.com>
2023-03-03 10:24:05 -0500

    add variable display_names for users

--- main.tf
+++ main.tf
@@ -27,3 +27,27 @@ resource "azuread_user" "trainer_user" {
   mail_nickname       = "iozbekler"
   password            = "SecretP@sswd99!"
 }
+
+# 4 users from for_each
+resource "azuread_user" "trainer_users_from_for_each" {
+  user_principal_name = "ibrahim.ozbekler@someemail.com"
+  display_name        = each.value
+  mail_nickname       = "iozbekler"
+  password            = "SecretP@sswd99!"
+  for_each            = toset(var.display_names)
+}
+
+variable "display_names" {
+  default = ["jon johnson", "tom Thomson", "bob bobson", "jan janson"]
+}
+
+# my user
+# trainer user 
+# 4 for_each users
+# 2 s3 buckets
+# resource group  on azure with a 
+# - vm
+# - storage account
+# 2 tags
+# 1 output variable
+# variables where possible
